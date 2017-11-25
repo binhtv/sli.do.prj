1421e2d0af3aaed272397a1e3afaa30f76b9bf1c
Binh Tran <binh12121989@gmail.com>
2017-11-25 21:10:23 +0700

    Live config build

--- sli.do/src/commons/configs.js
+++ sli.do/src/commons/configs.js
@@ -1,10 +1,10 @@
 const configs = {
-    apiUrl: 'http://localhost:1337/api',
+    apiUrl: `${process.env.REACT_APP_API_HOST ? process.env.REACT_APP_API_HOST : 'http://localhost'}:1337/api`,
     auth: {
         domain: 'sli.auth0.com',
-        clientId: 'SOSoMw3lAZ81V2R5ahBJQq2z_G_rqQEw',
-        callbackUrl: 'http://localhost:3000/callback'
+        clientId: `${process.env.REACT_APP_AUTH_CLIENTID?process.env.REACT_APP_AUTH_CLIENTID: 'SOSoMw3lAZ81V2R5ahBJQq2z_G_rqQEw'}`,
+        callbackUrl: `${process.env.REACT_APP_API_HOST ? process.env.REACT_APP_API_HOST : 'http://localhost'}:3000/callback`
     }
 }
-
+console.log(process.env);
 export default configs;
